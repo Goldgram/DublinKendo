9f28195846ee0b52cad4c1c8594376acb1828db2
Goldgram <willbrennandesign@gmail.com>
2015-06-21 22:18:19 +0100

    Slider added and adding from siteData object

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,50 +1,135 @@
-// console.log(siteData);
+/* map id's*/
+for (var i in siteData["homepageIdMapping"]) {
+  $("#"+i).text(siteData["homepageIdMapping"][i]);
+}
+var dojosString = "";
+/* add dojo content */
+var dojosString = "";
+var numOfDojos = siteData["dojoLocations"].length;
+for (var i = 0; i < numOfDojos; i++) {
+  var dojo = siteData["dojoLocations"][i];
+  dojosString += '<div class="locations floatLeft"><div class="contentPadding">';
+  dojosString += '<p class="boldFont fontSize20 marginBottom20">'+dojo["day"]+'</p>';
+  for (var j = 0; j < dojo["times"].length; j++) {
+    dojosString += '<p class="fontSize16Paragraph">'+dojo["times"][j]["type"]+'</p>';
+    dojosString += '<p class="boldFont fontSize16 colorBlue marginBottom20">'+dojo["times"][j]["time"]+'</p>';
+  }
+  dojosString += '<p class="fontSize16Paragraph">'+dojo["address"]+'</p>';
+  dojosString += '<a href="http://maps.google.com/?q='+dojo["googleAddress"]+'" target="_blank">';
+  dojosString += '<img class="mapImages" src="http://maps.googleapis.com/maps/api/staticmap?center='+dojo["googleLongLat"]+'&markers='+dojo["googleLongLat"]+'&zoom=16&size=560x300&sensor=false&scale=1">';
+  dojosString += '</a>';
+  dojosString += '<a href="http://maps.google.com/?q='+dojo["googleAddress"]+'" class="marginBottom20" target="_blank">';
+  dojosString += '<p class="boldFont fontSize16 colorBlue floatLeft">Google Maps</p>';
+  dojosString += '<div class="linkTriangle floatLeft"></div><div class="clearBoth"></div>';
+  dojosString += '</a>';
+  dojosString += '</div></div>';
+  if (i%2===1) {
+    dojosString += '<div class="clearBothLessThan768"></div>';
+  }
+}
+dojosString += '<div class="clearBoth"></div>';
+$("#dojosContent").html(dojosString);
 
-$(document).on("click", "#mobileButton", function() {
-	$("#mobileNavigation").stop(true, true).slideToggle();
+
+
+
+
+
+
+
+var siteData = {
+  "news":[
+    {"content":"whoop 1", "class":"contentBox1"}
+    ,{"content":"whoop 2", "class":"contentBox2"}
+    ,{"content":"whoop 3", "class":"contentBox1"}
+    ,{"content":"whoop 4", "class":"contentBox2"}
+    ,{"content":"whoop 5", "class":"contentBox1"}
+    ,{"content":"whoop 6", "class":"contentBox2"}
+    ,{"content":"whoop 7", "class":"contentBox1"}
+    ,{"content":"whoop 8", "class":"contentBox2"}
+    ,{"content":"whoop 9", "class":"contentBox1"}
+    ,{"content":"whoop 10", "class":"contentBox2"}
+
+  ]
+};
+
+var contentString = "";
+for (var index in siteData["news"]) {
+  contentString += "<li><div class='"+siteData["news"][index]["class"]+"'>"+siteData["news"][index]["content"]+"</div></li>";
+};
+$("#newsSlider ul").html(contentString);
+
+$(window).load(function() {
+  $("#newsSlider").flexslider({
+    animation:"slide"
+    // ,startAt:1
+    ,minItems: 1
+    ,maxItems: 3
+//, animationLoop: false,
+    ,itemWidth: 200
+    // ,itemMargin: 5
+  });
+});
+
+
+
+
+
+
+
+$(window).load(function() {
+  $(".parallaxContainer").css("background-color","transparent");
 });
 
 $(document).ready(function() {
-	var today = new Date();
-	// var dd = today.getDate();
-	$("#dateText").text(today.getDate());
-});
-$(window).load(function() {
-	$(".parallaxContainer").css("background-color","transparent");
+  var today = new Date();
+  $("#dateText").text(today.getDate());
 });
 
-// google maps api functions
-var geocoder;
-var map;
-function initializeGoogleMaps() {
-  geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
-  var mapOptions = {
-    zoom:16
-    ,center:latlng
-    ,panControl:true
-    ,mapTypeId:google.maps.MapTypeId.ROADMAP
-    ,streetViewControl:false
-  }
-  map = new google.maps.Map(document.getElementById('google-map-canvas'), mapOptions);
-}
-function codeAddress(address) {
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
-      var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location,
-          // icon: 'http://chart.apis.google.com/chart?chst=d_map_pin_letter&chld=%E2%80%A2|006980'
-      });
-    } else {
-      alert('Geocode was not successful for the following reason: ' + status);
-    }
+$(document).on("click", "#mobileButton", function() {
+  $("#mobileNavigation").stop(true, true).slideToggle();
+});
+
+var scrollSpeed = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ? 0.5 : 0.8;
+var scrollingToTop = false;
+$(document).on("click", "#scrollToTop", function() {
+  scrollingToTop = true;
+  $("html body").animate({ scrollTop: 0 }, $(window).scrollTop()*scrollSpeed,function(){
+    scrollingToTop = false;
   });
+  $("#scrollToTop").fadeOut("slow");
+});
+$(window).scroll(function(){
+  console.log("triggered");
+  if (scrollingToTop===false) {
+    if($(this).scrollTop()>50) {
+      $("#scrollToTop").fadeIn("slow");
+    } else {
+      $("#scrollToTop").fadeOut();
+    }
+  }
+})
+$(document).on("click", ".scrollToClick", function() {
+  var screenHeight = $(window).height();
+  var clickElement = $(this).attr("overlay");
+  var element = $("#"+$(this).attr("scroll-to"));
+  var elementHeight = element.outerHeight();
+  var elementOffsetTop = element.offset().top;
+  if (elementHeight<screenHeight) {
+    elementOffsetTop -= (screenHeight-elementHeight)/2;
+  }
+  $("html body").animate({ scrollTop: elementOffsetTop }, elementOffsetTop*scrollSpeed, function() {
+    clickElement && showOverlay(clickElement);
+  });
+});
+
+$(document).on("click", ".overlayClick", function() {
+  showOverlay($(this).attr("overlay"));
+});
+
+function showOverlay(overlay){
+  console.log("show overlay: "+overlay);
 }
-
-
-
 
 // function users(dataUrl,elementId,presentation, animate) {// users list class for multiple instances
 
